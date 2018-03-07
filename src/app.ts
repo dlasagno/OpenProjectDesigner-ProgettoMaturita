2a964544fb9a4ed5fc11e1968ef852f34f8d6801
Skyblaze1714 <danielelasagno@gmail.com>
2018-03-07 18:47:38 +0100

    Reorganized the project folder

--- src/app.ts
+++ src/app.ts
@@ -1,58 +1,59 @@
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
-const url = require('url')
-const {enableLiveReload} = require('electron-compile')
+import {app, BrowserWindow} from 'electron';
+import * as path from 'path';
+import * as url from 'url';
+import {enableLiveReload} from 'electron-compile';
 
-enableLiveReload()
+// Attiva l'aggiornamento automatico della finestra alla modifica di un file dell'app
+enableLiveReload();
 
 // Mantenere un riferimento globale dell'oggetto window, altrimenti la finestra verrà 
 // chiusa automaticamente quando l'oggetto JavaScript è raccolto nel Garbage Collector.
-let win
+let win;
 
 function createWindow () {
   // Creazione della finestra del browser.
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({width: 800, height: 600});
 
   // e viene caricato il file index.html della nostra app.
   win.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.jade'),
+    pathname: path.join(__dirname, '../index.jade'),
     protocol: 'file:',
     slashes: true
-  }))
+  }));
 
   // Apertura degli strumenti per sviluppatori.
-  win.webContents.openDevTools()
+  win.webContents.openDevTools();
 
   // Emesso quando la finestra viene chiusa.
   win.on('closed', () => {
     // Eliminiamo il riferimento dell'oggetto window;  solitamente si tiene traccia delle finestre
     // in array se l'applicazione supporta più finestre, questo è il momento in cui 
     // si dovrebbe eliminare l'elemento corrispondente.
-    win = null
-  })
+    win = null;
+  });
 }
 
 // Questo metodo viene chiamato quando Electron ha finito
 // l'inizializzazione ed è pronto a creare le finestre browser.
 // Alcune API possono essere utilizzate solo dopo che si verifica questo evento.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // Terminiamo l'App quando tutte le finestre vengono chiuse.
 app.on('window-all-closed', () => {
   // Su macOS è comune che l'applicazione e la barra menù 
   // restano attive finché l'utente non esce espressamente tramite i tasti Cmd + Q
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
 
 app.on('activate', () => {
   // Su macOS è comune ri-creare la finestra dell'app quando
   // viene cliccata l'icona sul dock e non ci sono altre finestre aperte.
   if (win === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
 
 // in questo file possiamo includere il codice specifico necessario 
 // alla nostra app. Si può anche mettere il codice in file separati e richiederlo qui.
