243ead922db864f4e4565c31fa581f4f9905579d
Skyblaze1714 <danielelasagno@gmail.com>
2018-03-22 18:00:49 +0100

    Updated the side menu look

--- test.ts
+++ test.ts
@@ -2,12 +2,12 @@ var opened = false;
 
 function ciao(){
   if(!opened){
-    document.getElementById("a").style.width ="200px";
+    document.getElementById("a").classList.add("active");
     //document.getElementById("b").style.marginLeft ="200px";
     document.getElementById("c").style.marginLeft =200-60+"px";
     opened=true;
   } else {
-    document.getElementById("a").style.width ="0px";
+    document.getElementById("a").classList.remove("active");
     //document.getElementById("b").style.marginLeft ="0px";
     document.getElementById("c").style.marginLeft ="0px";
     opened=false;
