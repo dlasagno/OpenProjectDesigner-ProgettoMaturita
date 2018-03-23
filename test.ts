4060f364e47dbe2b978b32a825f231c2f9145b4f
Frost Gear <danielelasagno@gmail.com>
2018-03-23 12:09:24 +0100

    Adjusted the GUI

--- test.ts
+++ test.ts
@@ -4,12 +4,12 @@ function ciao(){
   if(!opened){
     document.getElementById("a").classList.add("active");
     //document.getElementById("b").style.marginLeft ="200px";
-    document.getElementById("c").style.marginLeft =200-60+"px";
+    document.getElementById("c").classList.add("move-left");
     opened=true;
   } else {
     document.getElementById("a").classList.remove("active");
     //document.getElementById("b").style.marginLeft ="0px";
-    document.getElementById("c").style.marginLeft ="0px";
+    document.getElementById("c").classList.remove("move-left");
     opened=false;
   }
 }
