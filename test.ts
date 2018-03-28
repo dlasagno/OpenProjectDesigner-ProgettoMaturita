fae8fcdd9a6dfa1ee7abda76c2614ce0d2fe0a14
Skyblaze1714 <danielelasagno@gmail.com>
2018-03-28 22:30:21 +0200

    Did some test

--- test.ts
+++ test.ts
@@ -2,12 +2,12 @@ var opened = false;
 
 function ciao(){
   if(!opened){
-    document.getElementById("a").classList.add("active");
+    document.getElementById("side-menu").classList.add("active");
     //document.getElementById("b").style.marginLeft ="200px";
     document.getElementById("c").classList.add("move-left");
     opened=true;
   } else {
-    document.getElementById("a").classList.remove("active");
+    document.getElementById("side-menu").classList.remove("active");
     //document.getElementById("b").style.marginLeft ="0px";
     document.getElementById("c").classList.remove("move-left");
     opened=false;
