fdbd22aefe143665da44f472452f4bb56387b86d
Skyblaze1714 <danielelasagno@gmail.com>
2018-04-06 22:21:11 +0200

    Updated folder structure, updated style script

---
+++ src/scripts/style-scripts/style-script.ts
@@ -0,0 +1,8 @@
+const sideMenuToggler = document.querySelector('#side-menu-toggler');
+const sideMenu = document.querySelector('#side-menu');
+const tabNav = document.querySelector('#tab-nav');
+
+sideMenuToggler.addEventListener('click', () => {
+  sideMenu.classList.toggle('opened')
+  tabNav.classList.toggle('move-left');
+});
