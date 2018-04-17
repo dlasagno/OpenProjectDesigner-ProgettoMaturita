24c0080ab1e45687a41b69e815931544b989e917
Razvan <razvansalaru@gmail.com>
2018-04-17 10:51:01 +0200

    Prova con mxGraph

--- src/scripts/file_scripts/provafile.js
+++ src/scripts/file_scripts/provafile.js
@@ -1,4 +1,23 @@
 var fs = require('fs');
 var data = fs.readFileSync('../ProgettoEsame/src/data/project.json', 'utf-8');
 var project = JSON.parse(data);
-alert(project.number);
+
+mxBasePath = 'javascript/src';
+
+function main(container) {
+
+  var graph = new msGraph(container);
+
+  new mxRubberband(graph);
+
+  graph.getModel().beginUpdate();
+
+  try {
+    var v1 = graph.insertVertex(parent, null, "Hello", 20, 20, 80, 30);
+    var v2 = graph.insertVertex(parent, null, project.number, 20, 20, 80, 30);
+    var e1 = graph.insertEdge(parent, null, " ", v1, v2);
+  }
+  finally {
+    graph.getModel().endUpdate();
+  }
+}
