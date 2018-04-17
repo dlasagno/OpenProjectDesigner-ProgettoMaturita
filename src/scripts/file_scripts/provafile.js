3f20f1988235ce362c648a4ff30ecc538615d4c1
Razvan <razvansalaru@gmail.com>
2018-04-17 10:23:37 +0200

    Prova commit

--- src/scripts/file_scripts/provafile.js
+++ src/scripts/file_scripts/provafile.js
@@ -1,11 +1,4 @@
-$function() {
-  var people = [];
-
-  $.getJSON('src/data/project.json', function(data) {
-    $.each(data.task, function(i, f) {
-      var tblRow = "<tr><td>" + f.number + "</td><td>" + f.title + "</td><td>" + f.start_date + "</td><td>" + f.end_date + "</td></tr>";
-      $(tblRow).appendTo("#userdata tbody");
-
-    });
-  });
-});
+var fs = require('fs');
+var data = fs.readFileSync('../ProgettoEsame/src/data/project.json', 'utf-8');
+var project = JSON.parse(data);
+alert(project.number);
