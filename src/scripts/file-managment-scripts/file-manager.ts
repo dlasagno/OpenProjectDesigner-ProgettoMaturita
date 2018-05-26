90cb73474dacd144b7114c9f8b65ab168acea607
Skyblaze1714 <danielelasagno@gmail.com>
2018-05-26 17:11:55 +0200

    Resolved problem with raphael library

--- src/scripts/file-managment-scripts/file-manager.ts
+++ src/scripts/file-managment-scripts/file-manager.ts
@@ -1,5 +1,8 @@
+import { Tree, Task } from '../commons/interfaces'
 import * as fs from 'fs'
 
-export class FileManager {
-  
+export const FileManager = {
+  toFile(tasks: Tree<Task>) {
+    
+  }
 }
--- src/scripts/tab-window-scripts/app.ts
+++ src/scripts/tab-window-scripts/app.ts
@@ -569,7 +569,7 @@ const tabController = new TabController([
       const paper = Raphael(0 , 0, 500, 400)
       const rect = paper.rect(1, 1, 20, 30).attr('fill', 'red')
     
-      return paper
+      return document.createElement('div')
     }
   },
   {
