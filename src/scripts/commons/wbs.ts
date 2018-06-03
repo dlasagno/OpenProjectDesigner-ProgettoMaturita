36c329f59f41defa0c0ab4f9eda85d02b03de67d
Dioraz <razvansalaru@gmail.com>
2018-06-03 21:27:05 +0200

    update the wbs
    add event click

--- src/scripts/commons/wbs.ts
+++ src/scripts/commons/wbs.ts
@@ -7,9 +7,40 @@ const ganttTab: Tab = {
   icon: 'fa-sitemap',
   menuItems: [],
   view(tabController: TabController): Element {
-    const paper = Raphael(0 , 0, 500, 400)
-    const rect = paper.rect(1, 1, 20, 30).attr('fill', 'red')
+    const wbsElement = document.createElement('div')
+    const paper = Raphael(wbsElement, 1000, 1000)
 
-    return paper
+    let count = 1
+    const taskCoo = tabController.tasks.map<{x: number, y: number}>((node, id) => {
+      return new TreeNode({
+        x: id ? id.split('.').length * 130 : 10,
+        y: 80 * count++
+      })
+    })
+
+    function createRect(x: number, y: number, id: string){
+      const rect = paper.rect(x, y, 100, 50).attr({"fill": "white", "cursor": "pointer"}).click(function() {
+        tabController.selectedTaskId = id
+      }) 
+    }
+    
+    function createLine(startX: number, startY: number, finishX: number, finishY: number){
+      const path = paper.path(`M ${startX} ${startY} L ${finishX} ${startY} ${finishX} ${finishY}`)
+    }
+
+    function createTitle(text: string, x: number, y: number){
+      const title = paper.text(x, y, text).attr({"font-size": 18, "text-anchor": "start"})
+     }
+
+
+    taskCoo.forEach((task, id) => { 
+      createRect(task.data.x, task.data.y, id)
+      //createTitle(tabController.tasks.getNodeById(id).data.title, (task.data.x + 10), (task.data.y))
+      if(id != '')
+        createLine(task.data.x, (task.data.y + 25), (taskCoo.getNodeById(id.split('.').slice(0, -1).join('.')).data.x + 50), (taskCoo.getNodeById(id.split('.').slice(0, -1).join('.')).data.y) + 50)
+    })  
+    
+    
+    return wbsElement
   }
 }
--- src/scripts/tab-window-scripts/app.ts
+++ src/scripts/tab-window-scripts/app.ts
@@ -600,30 +600,30 @@ const tabController = new TabController([
         })
       })
 
-      function createRect(x: number, y: number){
-        const rect = paper.rect(x, y, 100, 50)
+      function createRect(x: number, y: number, id: string){
+        const rect = paper.rect(x, y, 100, 50).attr({"fill": "white", "cursor": "pointer"}).click(function() {
+          tabController.selectedTaskId = id
+        }) 
       }
       
       function createLine(startX: number, startY: number, finishX: number, finishY: number){
         const path = paper.path(`M ${startX} ${startY} L ${finishX} ${startY} ${finishX} ${finishY}`)
       }
+
+      function createTitle(text: string, x: number, y: number){
+        const title = paper.text(x, y, text).attr({"font-size": 18, "text-anchor": "start"})
+       }
   
 
       taskCoo.forEach((task, id) => { 
-        
-        createRect(task.data.x, task.data.y)
+        createRect(task.data.x, task.data.y, id)
+        //createTitle(tabController.tasks.getNodeById(id).data.title, (task.data.x + 10), (task.data.y))
         if(id != '')
           createLine(task.data.x, (task.data.y + 25), (taskCoo.getNodeById(id.split('.').slice(0, -1).join('.')).data.x + 50), (taskCoo.getNodeById(id.split('.').slice(0, -1).join('.')).data.y) + 50)
-        
-      })
-      
-      
-
-      
+      })  
       
       
       return wbsElement
-
     }
   },
   {
