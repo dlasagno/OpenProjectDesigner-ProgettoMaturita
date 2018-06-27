00638bf5919b1edd3a0611b69783e85810ffebd1
Skyblaze1714 <danielelasagno@gmail.com>
2018-06-27 15:45:54 +0200

    Cleaned up projectInfoTab cod

--- src/scripts/commons/project-info.ts
+++ src/scripts/commons/project-info.ts
@@ -1,19 +1,33 @@
 import { TabController } from '../tab-window-scripts/tab-controller'
-import { Property, Task, Tab } from './interfaces'
+import { Tab } from './interfaces'
 
 
 
 export const projectInfoTab: Tab = {
-    name: 'Info',
+    name: 'info',
     icon: 'fa-info-circle',
     menuItems: [],
     view(tabController: TabController): Element {
+
+        function createCell(cellValue: string, onEnterKeyDown: (event: KeyboardEvent) => void): HTMLTableDataCellElement {
+            const cell = document.createElement('td')
+            const cellContent = document.createElement('div')
+                cellContent.textContent = cellValue
+                cellContent.setAttribute('contenteditable', 'true')
+                cellContent.addEventListener('keydown', event => {
+                    if (event.key === "Enter") {
+                        onEnterKeyDown(event)
+                    }
+                })
+            cell.appendChild(cellContent)
+            return cell
+        }
         
         const projectInfoElement = document.createElement('div')
         const table = document.createElement('table')
-    
-        const infoHeader = document.createElement('tr')
-              infoHeader.innerHTML = `
+        
+        const projectInfoHeader = document.createElement('tr')
+              projectInfoHeader.innerHTML = `
                 <th>Title</th>
                 <th>Cost</th>
                 <th>Progress</th>
@@ -21,101 +35,53 @@ export const projectInfoTab: Tab = {
                 <th>End Date</th>
                 <th>Description</th>
               `
-                
-        const infoContent = document.createElement('tr')
-            
-        //td for the title
-        const title = document.createElement('td')
-        const titleContent = document.createElement('div')
-              titleContent.innerHTML = tabController.tasks.root.data.title
-              titleContent.setAttribute('contenteditable', 'true')
-              titleContent.addEventListener('keydown', event => {
-                if (event.key === "Enter") {
-                    const value = titleContent.innerHTML
-                    tabController.tasks.root.data.title = value
-                    tabController.update()
-                }
-              })
-        title.appendChild(titleContent)
-        infoContent.appendChild(title)
+        
+        const projectInfoContent = document.createElement('tr')
 
-        //td for the cost
-        const cost = document.createElement('td')
-        const costContent = document.createElement('div')
-              costContent.innerHTML = tabController.tasks.root.data.cost.toString()
-              costContent.setAttribute('contenteditable', 'true')
-              costContent.addEventListener('keydown', event => {
-                if (event.key === "Enter") {
-                    const value = costContent.innerHTML
-                    tabController.tasks.root.data.cost = parseInt(value)
-                    tabController.update()
-                }
-              })
-        cost.appendChild(costContent)
-        infoContent.appendChild(cost)
+        //Create the cell for the title
+        projectInfoContent.appendChild(createCell(tabController.tasks.root.data.title, event => {
+            const value = (event.target as Element).textContent
+            tabController.tasks.root.data.title = value
+            tabController.update()
+        }))
 
-        //td for the progress
-        const progress = document.createElement('td')
-        const progressContent = document.createElement('div')
-              progressContent.innerHTML = tabController.tasks.root.data.progress.toString()
-              progressContent.setAttribute('contenteditable', 'true')
-              progressContent.addEventListener('keydown', event => {
-                if (event.key === "Enter") {
-                    const value = progressContent.innerHTML
-                    tabController.tasks.root.data.progress = parseInt(value)
-                    tabController.update()
-                }
-              })
-        progress.appendChild(progressContent)
-        infoContent.appendChild(progress)
+        //Create the cell for the cost
+        projectInfoContent.appendChild(createCell(tabController.tasks.root.data.cost.toString(), event => {
+            const value = (event.target as Element).textContent
+            tabController.tasks.root.data.cost = parseInt(value)
+            tabController.update()
+        }))
 
-        //td for the start_date
-        const startDate = document.createElement('td')
-        const startDateContent = document.createElement('div')
-              startDateContent.innerHTML = tabController.tasks.root.data.start_date.toLocaleDateString()
-              startDateContent.setAttribute('contenteditable', 'true')
-              startDateContent.addEventListener('keydown', event => {
-                if (event.key === "Enter") {
-                    const value = startDateContent.innerHTML
-                    tabController.tasks.root.data.start_date = new Date(value)
-                    tabController.update()
-                }
-              })
-        startDate.appendChild(startDateContent)
-        infoContent.appendChild(startDate)
+        //Create the cell for the progress
+        projectInfoContent.appendChild(createCell(tabController.tasks.root.data.progress.toString(), event => {
+            const value = (event.target as Element).textContent
+            tabController.tasks.root.data.progress = parseInt(value)
+            tabController.update()
+        }))
 
-        //td for the end_date
-        const endDate = document.createElement('td')
-        const endDateContent = document.createElement('div')
-              endDateContent.innerHTML = tabController.tasks.root.data.end_date.toLocaleDateString()
-              endDateContent.setAttribute('contenteditable', 'true')
-              endDateContent.addEventListener('keydown', event => {
-                if (event.key === "Enter") {
-                    const value = endDateContent.innerHTML
-                    tabController.tasks.root.data.end_date = new Date(value)
-                    tabController.update()
-                }
-              })
-        endDate.appendChild(endDateContent)
-        infoContent.appendChild(endDate)
+        //Create the cell for the start date
+        projectInfoContent.appendChild(createCell(tabController.tasks.root.data.start_date.toString(), event => {
+            const value = (event.target as Element).textContent
+            tabController.tasks.root.data.start_date = new Date(value)
+            tabController.update()
+        }))
 
-        //td for the description
-        const description = document.createElement('td')
-        const descriptionContent = document.createElement('div')
-              descriptionContent.innerHTML = tabController.tasks.root.data.description
-              descriptionContent.setAttribute('contenteditable', 'true')
-              descriptionContent.addEventListener('keydown', event => {
-                if (event.key === "Enter") {
-                    const value = descriptionContent.innerHTML
-                    tabController.tasks.root.data.description = value
-                    tabController.update()
-                }
-              })
-        description.appendChild(descriptionContent)
-        infoContent.appendChild(description)
+        //Create the cell for the end date
+        projectInfoContent.appendChild(createCell(tabController.tasks.root.data.end_date.toString(), event => {
+            const value = (event.target as Element).textContent
+            tabController.tasks.root.data.end_date = new Date(value)
+            tabController.update()
+        }))
 
-        table.appendChild(infoHeader)
-        table.appendChild(infoContent)
+        //Create the cell for the description
+        projectInfoContent.appendChild(createCell(tabController.tasks.root.data.description, event => {
+            const value = (event.target as Element).textContent
+            tabController.tasks.root.data.description = value
+            tabController.update()
+        }))
+
+        table.appendChild(projectInfoHeader)
+        table.appendChild(projectInfoContent)
         projectInfoElement.appendChild(table)
     
         return projectInfoElement
