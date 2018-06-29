799bd56ab9a60b2482edd89b32d65c0eb22c9d7b
Skyblaze1714 <danielelasagno@gmail.com>
2018-06-29 12:18:30 +0200

    Now when the app starts ask you to open a project or create one

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -15,190 +15,199 @@ import '../../node_modules/@fortawesome/fontawesome-free/css/all.css'
 import './style-scripts/style-script'
 
 
-//Tasks for testing the software
-const tasks: Tree<Task> = new Tree({
-  title: 'Progetto',
-  description: 'desrizione progetto',
-  collapsed: false,
-  start_date: new Date('2018-01-01'),
-  end_date: new Date('2018-01-03'),
-  progress: 50,
-  cost: 3000
-})
-tasks.root.appendChildren([
-  {
-    title: 'Pianificazione',
-    description: 'descrizione pianificazione',
-    collapsed: false,
-    start_date: new Date('2018-01-03'),
-    end_date: new Date('2018-01-03'),
-    progress: 50,
-    cost: 500
-  },
-  {
-    title: 'Preparazione',
-    description: 'descrizione preparazione',
-    collapsed: false,
-    start_date: new Date('2018-01-03'),
-    end_date: new Date('2018-01-03'),
-    progress: 30,
-    cost: 2000
-  },
-  {
-    title: 'Realizzazione',
-    description: 'descrizione realizzazione',
-    collapsed: false,
-    start_date: new Date('2018-01-03'),
-    end_date: new Date('2018-01-03'),
-    progress: 70,
-    cost: 5000
-  }
-])
-tasks.getNodeById('1').appendChildren([
-  {
-    title: 'Raccolta dati',
-    description: 'descrizione raccolta dati',
-    collapsed: false,
-    start_date: new Date('2018-01-01'),
-    end_date: new Date('2018-01-01'),
-    progress: 50,
-    cost: 100
-  },
-  {
-    title: 'Esaminazione',
-    description: 'descrizione esaminazione',
-    collapsed: false,
-    start_date: new Date('2018-01-02'),
-    end_date: new Date('2018-01-03'),
-    progress: 50,
-    cost: 400
-  }
-])
-tasks.getNodeById('1.1').appendChildren([
-  {
-    title: 'Raccolta richieste clienti',
-    description: 'descrizione raccolta dati',
-    collapsed: false,
-    start_date: new Date('2018-01-01'),
-    end_date: new Date('2018-01-01'),
-    progress: 80,
-    cost: 30
-  },
-  {
-    title: 'Raccolta esigenze utenti',
-    description: 'descrizione esaminazione',
-    collapsed: false,
-    start_date: new Date('2018-01-01'),
-    end_date: new Date('2018-01-01'),
-    progress: 20,
-    cost: 70
-  }
-])
-
 //Management of the files
-//FileManager.toFile('src/data/prova.json', tasks)
-const tasksFromFile: Tree<Task> = FileManager.fromFile('src/data/prova.json')
-let currentFile: string = 'src/data/prova.json'
+let currentFile: string
 
 //Create a modal for testing
 const modalController = new ModalController()
+
+//Inizializzazione del tabController
+let tabController
+if(FileManager.filesFromFolder('src/data').length){
+  const modalHeader = document.createElement('span')
+    modalHeader.textContent = 'Choose the project to open'
+
+  const modalBody = document.createElement('div')
+    modalBody.classList.add('list')
+
+  FileManager.filesFromFolder('src/data').forEach(file => {
+    const fileDiv = document.createElement('button')
+          fileDiv.classList.add('button')
+          fileDiv.textContent = file.split('.')[0].replace('-', ' ')
+          fileDiv.addEventListener('click', () => {
+            currentFile = `src/data/${file}`
+            const tasks = FileManager.fromFile(currentFile)
+            tabController = new TabController([wbsTab, ganttTab, projectInfoTab], tasks)
+            tabController.update()
+            modalController.closeModal()
+          })
+    modalBody.appendChild(fileDiv)
+  })
+
+  modalController.createModal({
+    header: modalHeader,
+    body: modalBody,
+    canBeClosed: false
+  })
+}
+else {
+  const modalHeader = document.createElement('span')
+    modalHeader.textContent = 'Insert the name of the project'
+
+  const inputText = document.createElement('input') 
+    inputText.setAttribute('type', 'text')
+    inputText.required = true
+    inputText.addEventListener('keydown', event => {
+      if(event.key === 'Enter'&& inputText.value){
+        FileManager.toFile(`src/data/${inputText.value}.json`, new Tree<Task>({ 
+          title: inputText.value, 
+          description: '', 
+          collapsed: false, 
+          start_date: new Date('2018-01-01'), 
+          end_date: new Date('2018-01-01'), 
+          progress: 0, 
+          cost: 0 
+        })) 
+        currentFile = `src/data/${inputText.value}.json`
+        const tasks = FileManager.fromFile(currentFile)
+        tabController = new TabController([wbsTab, ganttTab, projectInfoTab], tasks)
+        tabController.update() 
+        modalController.closeModal()
+      }
+    })
+
+  const modalBody = document.createElement('p') 
+    modalBody.textContent = 'Project name: ' 
+    modalBody.appendChild(inputText)
+    
+  const modalFooter = document.createElement('button')
+    modalFooter.classList.add('button')
+    modalFooter.textContent = 'New' 
+    modalFooter.addEventListener('click', () => {
+      if(inputText.value){
+        FileManager.toFile(`src/data/${inputText.value}.json`, new Tree<Task>({ 
+          title: inputText.value, 
+          description: '', 
+          collapsed: false, 
+          start_date: new Date('2018-01-01'), 
+          end_date: new Date('2018-01-01'), 
+          progress: 0, 
+          cost: 0 
+        })) 
+        currentFile = `src/data/${inputText.value}.json` 
+        tabController.tasks = FileManager.fromFile(currentFile) 
+        tabController.update() 
+        modalController.closeModal()
+      }
+    }) 
+  
+  modalController.createModal({ 
+    header: modalHeader, 
+    body: modalBody,
+    footer: modalFooter,
+    canBeClosed: false
+  })
+}
+
+//Side menu actions
+function openProjectModal() {
+  const modalHeader = document.createElement('span')
+    modalHeader.textContent = 'Choose the project to open'
+
+  const modalBody = document.createElement('div')
+    modalBody.classList.add('list')
+
+  FileManager.filesFromFolder('src/data').forEach(file => {
+    const fileDiv = document.createElement('button')
+          fileDiv.classList.add('button')
+          fileDiv.textContent = file.split('.')[0].replace('-', ' ')
+          fileDiv.addEventListener('click', () => {
+            currentFile = `src/data/${file}`
+            const tasks = FileManager.fromFile(currentFile)
+            tabController.tasks = tasks
+            tabController.update()
+            modalController.closeModal()
+          })
+    modalBody.appendChild(fileDiv)
+  })
+
+  modalController.createModal({
+    header: modalHeader,
+    body: modalBody
+  })
+}
+function saveProjectModal() {
+  FileManager.toFile(currentFile, tabController.tasks)
+  
+  const modalHeader = document.createElement('span')
+    modalHeader.textContent = 'Project saved'
+
+  const modalBody = document.createElement('span')
+    modalBody.textContent = 'The current project has been saved'
+  
+  modalController.createModal({
+    header: modalHeader,
+    body: modalBody
+  })
+}
+function newProjectModal() {
+  const modalHeader = document.createElement('span')
+    modalHeader.textContent = 'Insert the name of the project'
+
+  const inputText = document.createElement('input') 
+    inputText.setAttribute('type', 'text')
+    inputText.required = true
+    inputText.addEventListener('keydown', event => {
+      if(event.key === 'Enter')
+        createProject()
+    })
+
+  const modalBody = document.createElement('p') 
+    modalBody.textContent = 'Project name: ' 
+    modalBody.appendChild(inputText)
+    
+  const modalFooter = document.createElement('button')
+    modalFooter.classList.add('button')
+    modalFooter.textContent = 'New' 
+    modalFooter.addEventListener('click', createProject) 
+  
+  modalController.createModal({ 
+    header: modalHeader, 
+    body: modalBody,
+    footer: modalFooter
+  })
+
+  function createProject(){
+    if(inputText.value){
+      FileManager.toFile(`src/data/${inputText.value}.json`, new Tree<Task>({ 
+        title: inputText.value, 
+        description: '', 
+        collapsed: false, 
+        start_date: new Date('2018-01-01'), 
+        end_date: new Date('2018-01-01'), 
+        progress: 0, 
+        cost: 0 
+      })) 
+      currentFile = `src/data/${inputText.value}.json` 
+      tabController.tasks = FileManager.fromFile(currentFile) 
+      tabController.update() 
+      modalController.closeModal()
+    }
+  }
+}
 
 //Load the side menu
 const sideMenuController = new SideMenuController([
   {
     name: 'Open project',
-    action() {
-      const modalHeader = document.createElement('span')
-        modalHeader.textContent = 'Choose the project to open'
-
-      const modalBody = document.createElement('div')
-        modalBody.classList.add('list')
-
-      FileManager.filesFromFolder('src/data').forEach(file => {
-        const fileDiv = document.createElement('button')
-              fileDiv.classList.add('button')
-              fileDiv.textContent = file.split('.')[0].replace('-', ' ')
-              fileDiv.addEventListener('click', () => {
-                currentFile = `src/data/${file}`
-                tabController.tasks = FileManager.fromFile(currentFile)
-                tabController.update()
-                modalController.closeModal()
-              })
-        modalBody.appendChild(fileDiv)
-      })
-
-      modalController.createModal({
-        header: modalHeader,
-        body: modalBody
-      })
-    }
+    action: openProjectModal
   },
   {
     name: 'Save project',
-    action() {
-      FileManager.toFile(currentFile, tabController.tasks)
-      
-      const modalHeader = document.createElement('span')
-        modalHeader.textContent = 'Project saved'
-
-      const modalBody = document.createElement('span')
-        modalBody.textContent = 'The current project has been saved'
-      
-      modalController.createModal({
-        header: modalHeader,
-        body: modalBody
-      })
-    }
+    action: saveProjectModal
   },
   {
     name: 'New project',
-    action(){
-      const modalHeader = document.createElement('span')
-        modalHeader.textContent = 'Insert the name of the project'
-
-      const inputText = document.createElement('input') 
-        inputText.setAttribute('type', 'text')
-        inputText.required = true
-        inputText.addEventListener('keydown', event => {
-          if(event.key === 'Enter')
-            createProject()
-        })
-
-      const modalBody = document.createElement('p') 
-        modalBody.textContent = 'Project name: ' 
-        modalBody.appendChild(inputText)
-        
-      const modalFooter = document.createElement('button')
-        modalFooter.classList.add('button')
-        modalFooter.textContent = 'New' 
-        modalFooter.addEventListener('click', createProject) 
-      
-      modalController.createModal({ 
-        header: modalHeader, 
-        body: modalBody,
-        footer: modalFooter
-      })
-
-      function createProject(){
-        if(inputText.value){
-          FileManager.toFile(`src/data/${inputText.value}.json`, new Tree({ 
-            title: inputText.value, 
-            description: '', 
-            collapsed: false, 
-            start_date: new Date('2018-01-01'), 
-            end_date: new Date('2018-01-03'), 
-            progress: 0, 
-            cost: 0 
-          })) 
-          currentFile = `src/data/${inputText.value}.json` 
-          tabController.tasks = FileManager.fromFile(currentFile) 
-          tabController.update() 
-          modalController.closeModal()
-        }
-      }
-    }
+    action: newProjectModal
   }
 ])
-
-//Load the tab window
-const tabController = new TabController([wbsTab, ganttTab, projectInfoTab], tasksFromFile)
